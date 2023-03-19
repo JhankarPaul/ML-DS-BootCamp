6482c08a8a000753da9831adde514a5b58285d0d
JhankarPaul <jhankarspaul@gmail.com>
2023-03-19 13:37:53 +0600

    initial commit

--- variables.py
+++ variables.py
@@ -2,8 +2,16 @@ x=5
 y=10.5
 z="Shatabdi"
 print("please assign something to a : ")
-a=input() #user input
+a=int(input()) #user input as integer
+b=float(input()) #user input as floating point
+c=input() #user input
+
+# REMEMBER: input() takes string. int(input()) makes the string integer.
+
 print(x,y)
-print(a)
+print(a,b,c)
 print(type(x),type(y),type(z))
-print(type(a))
+print(type(a),type(b),type(c))
+
+sum=a+b
+print(sum)
